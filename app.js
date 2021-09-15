47c7363a11011cf5d17d27a322ae685281288291
Christina Aiello <caiello@datto.com>
2021-09-15 02:22:43 +0000

    Hahahahahahahahahahahahahaha oh god I need to go to sleep

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 //app.use(ipfilter(ips, { mode: 'allow' }));
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -25,36 +25,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', indexRouter);
 
-app.get('/', function(req, res, next) {
+app.use('/', function(req, res, next) {
   const ips = ['8.41.72.157','8.33.72.250','8.41.72.250','47.19.105.250','8.12.58.0']
   var host = req.headers['host'];
   console.log(`Host is ${host}`);
   var forwardedIpsStr = req.header('x-forwarded-for'); 
   console.log(`forwardedIpsStr is ${forwardedIpsStr}`);
-  res.render('index', { title: 'Express' });
   if (!ips.includes(forwardedIpsStr)) {
     console.log(`!ips.includes(forwardedIpsStr):  ${ips} does not include ${forwardedIpsStr}`);
-    res.end();
+    res.render('error');
   } else {
     console.log(`ips.includes(forwardedIpsStr):  ${ips} does include ${forwardedIpsStr}`);
     return indexRouter;
   }
 });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
