38d5fb8de7d81b21ee3f362730c9d64d03752b89
Christina Aiello <caiello@datto.com>
2021-09-15 02:02:48 +0000

    Hacky way of only allowing internal IPs

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const ips = ['8.41.72.157','8.33.72.250','8.41.72.250','47.19.105.250','8.12.58.0']
-  var host = req.headers['host'];
-  console.log(`Host is ${host}`);
-  var forwardedIpsStr = req.header('x-forwarded-for'); 
-  console.log(`forwardedIpsStr is ${forwardedIpsStr}`);
-  res.render('index', { title: 'Express' });
-  if (!ips.includes(forwardedIpsStr)) {
-    createError(404);
-  }
+  console.log(`router.get('/', function(req, res, next)`);
 });
 
 module.exports = router;
