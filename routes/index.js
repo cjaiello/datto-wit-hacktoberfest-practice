7d2eefd2df4d206f14e86a665e7016803ae9beeb
Christina Aiello <caiello@datto.com>
2021-09-15 01:21:19 +0000

    Attempting to get actual IP

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var host = req.headers['host'];
-  console.log(host);
+  console.log(`Host is ${host}`);
   res.render('index', { title: 'Express' });
 });
 
